dd92a16962a744b808fcf09340e9bde1ba3e0f4f
andrellferreira <78090005+andrellferreira@users.noreply.github.com>
2024-09-11 12:04:16 -0300

    listar metas

--- index.js
+++ index.js
@@ -14,7 +14,60 @@ tentei instalar e deu erro. Usei o comando npm cache clean (não funcionou). Use
 */
 // Opção 02 - Arrow Function, atribuindo a uma constante
 
-const { select } = require("@inquirer/prompts");
+//const { select } = require("@inquirer/prompts");] - V0
+//input acrescentada para capturar dados fornecidos pelo usuário - funçao cadastrarMeta() - V1
+//const { select, input } = require("@inquirer/prompts");
+//checkbox inserido para usar na função listarMetas()
+const { select, input, checkbox } = require("@inquirer/prompts");
+
+let meta = {
+  value: "Tomar 3L de água diariamente",
+  checked: false,
+};
+
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  const meta = await input({ message: "Digite a meta:" });
+
+  if (meta.length == 0) {
+    console.log("A meta não pode ser vazia!");
+    return; // Assim, retornará para o menu. Se eu quisesse que o usuário colocasse novamente uma meta: return cadastrarMeta()
+    // return cadastrarMeta(); // Teste OK
+  }
+  metas.push({
+    value: meta,
+    checked: false,
+  });
+};
+
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message:
+      "Uses as setas para mudar de meta, o espaço para marcar ou desmacar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  });
+
+  if (respostas.length == 0) {
+    console.log("Nenhuma meta selecionada");
+    return;
+  }
+
+  metas.forEach((m) => {
+    m.checked = false;
+  });
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+
+    meta.checked = true;
+  });
+
+  console.log("Meta(s) marcadas como concluídas");
+};
+
 const start = async () => {
   //let opcao = "cadastrar";//loop infinito
   //let opcao = "sair";
@@ -39,10 +92,11 @@ const start = async () => {
     }); //await - aguardar -> sempre que for usada, a funçao precisará usar async
     switch (opcao) {
       case "cadastrar":
-        console.log("Vamos cadastrar");
+        await cadastrarMeta();
+        console.log(metas);
         break;
       case "listar":
-        console.log("Vamos listar");
+        await listarMetas();
         break;
       case "sair":
         console.log("Até a próxima!");
